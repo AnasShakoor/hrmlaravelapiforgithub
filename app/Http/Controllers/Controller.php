2b54fc4a096475b51d281d216081f44b48a535d5
AnasShakoor <147330270+AnasShakoor@users.noreply.github.com>
2024-01-30 10:35:24 +0500

    changed the issues

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+use OpenApi\Annotations as OA;
+    
+/**
+ * @OA\Info(
+ *     title="Your API",
+ *     version="1.0.0",
+ *     description="API documentation for Your Laravel application",
+ *     @OA\Contact(
+ *         email="your@email.com",
+ *         name="Your Name"
+ *     ),
+ *     @OA\License(
+ *         name="Your License",
+ *         url="http://your-license-url.com"
+ *     )
+ * )
+ */
 class Controller extends BaseController
 {
+
     use AuthorizesRequests, ValidatesRequests;
 }
